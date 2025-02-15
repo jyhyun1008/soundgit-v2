a0fb875003af47d5e19a358656163de4770154d8
jyhyun1008 <jayhyun1008@gmail.com>
2025-02-16 08:58:37 +0900

    About 페이지에 README가 들어가도록

--- components/About.js
+++ components/About.js
@@ -1,12 +1,22 @@
 'use client'
+import { useState } from "react";
+import MarkedParser from "./MarkedParser";
 
 export default function About() {
+
+    const [raw, setRaw] = useState('')
     
+    fetch(`https://raw.githubusercontent.com/${process.env.NEXT_PUBLIC_GITHUB}/${process.env.NEXT_PUBLIC_REPO}/main/README.md`)
+    .then(res => res.text())
+    .then((out) => {
+        setRaw(out)
+    })
+
     return (
         <>
             <div id="player-list" className="mainbox">
                 <div id="player-overflow">
-                <p>SoundGit에 대해서~</p>
+                <MarkedParser raw={raw}/>
                 </div>
             </div>
         </>
